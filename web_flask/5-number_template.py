b981ce21040164ecae4c88013434e02c71ebeea9
shaimaayosef <shimaa.sayed8888@gmail.com>
2024-03-24 21:45:13 +0200

    2

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,40 +1,54 @@
 #!/usr/bin/python3
-""" 4. Add fourth view function that displays var only if is integer """
+""" 5. Add fifth view func that displays HTML page if n is int """
 
-from flask import Flask,
+from flask import Flask
 from flask import render_template
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route('/')
-def hello():
+def hello_world():
+    """ Returns some text. """
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb')
-def hbnb():
+def hello():
+    """ Return other text. """
     return 'HBNB'
+
 
 @app.route('/c/<text>')
 def c_text(text):
-    text = text.replace('_',' ')
+    """ replace text with variable. """
+    text = text.replace('_', ' ')
     return 'C {}'.format(text)
+
 
 @app.route('/python/')
 @app.route('/python/<text>')
 def python_text(text='is cool'):
-    text = text.replace('_',' ')
-    return 'python {}'.format(text)
+    """ replace more text with another variable. """
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
 
 @app.route('/number/<int:n>')
-def number_n(n):
+def number_text(n):
+    """ replace with int only if given int. """
     n = str(n)
     return '{} is a number'.format(n)
 
+
 @app.route('/number_template/<int:n>')
-def number_template_n(n):
+def html_num(n):
+    """ display html if n is int. """
     n = str(n)
     return render_template('5-number.html', n=n)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
