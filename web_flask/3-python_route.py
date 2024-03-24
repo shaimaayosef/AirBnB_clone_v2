8a718989e3f8f1ea202ac6896145b65b14d2f395
shaimaayosef <shimaa.sayed8888@gmail.com>
2024-03-24 21:16:44 +0200

    1

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" 3. Add third view func that redirects and has default val for variable """
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    return 'C %s' % text.replace('_', ' ')
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    return 'Python %s' % text.replace('_', ' ')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
